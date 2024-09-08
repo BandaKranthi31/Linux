1477144277ee4429f6d2c8faea191a61183975c0
Banda Kranthi Kumar <162495729+BandaKranthi31@users.noreply.github.com>
2024-09-08 20:38:16 +0530

    added some animation

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry);
         if(entry.isIntersecting){
             entry.target.classList.add('show');
-        } else{
-            entry.target.classList.remove('show');
         }
+       
     });
 });
 
@@ -19,7 +18,8 @@ const observer2 = new IntersectionObserver((entries) => {
         console.log(entry);
         if(entry.isIntersecting){
             entry.target.classList.add('show2');
-        } else{
+        }
+         else{
             entry.target.classList.remove('show2');
         }
     });
@@ -27,3 +27,20 @@ const observer2 = new IntersectionObserver((entries) => {
 
 const hiddenElements2 = document.querySelectorAll('.animate2');
 hiddenElements2.forEach((el2) => observer2.observe(el2));
+
+
+
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show3');
+        }
+         else{
+            entry.target.classList.remove('show3');
+        }
+    });
+});
+
+const hiddenElements3 = document.querySelectorAll('.hidden3');
+hiddenElements3.forEach((el3) => observer3.observe(el3));
